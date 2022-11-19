b5cca403c80a242d9e3ff3553723dce9a0237f75
Andrey Kulikov <aa_kylukov@vk.com>
2022-11-20 00:27:36 +0300

    Add 2 homework

---
+++ 2 homework/Car.cs
@@ -0,0 +1,105 @@
+﻿namespace MyApp;
+
+    public class Car
+    {
+        private string _name;
+        private int _hp_power;
+        private int _year;
+
+
+        public string Name { get => _name; set => _name = value; }
+
+        public int Horse_Power { get => _hp_power; set => _hp_power = value; }
+
+        public int Year { get => _year; set => _year = value; }
+
+
+
+
+        public Car(string name, int hp_power, int year) { _name = name; _hp_power = hp_power; _year = year; }
+
+         public override string ToString()
+        {
+            return $"object: {_name} {_hp_power} {_year}";
+        }
+
+    }
+
+public class PassengerCar : Car
+{
+    private int _AmountOfPasangers;
+    private Dictionary<string, int> _service_book = new Dictionary<string, int>();
+
+    public PassengerCar(string name, int hp_power, int year, int passengers) : base(name, hp_power, year)
+    {
+        _AmountOfPasangers = passengers;
+    }
+
+    public void AddToServiceBook(int year, string name)
+    {
+        _service_book.Add(name, year);
+    }
+
+    public void ShowServiceBook()
+    {
+        foreach (var element in _service_book)
+        {
+            Console.WriteLine($"> {element.Key} year: {element.Value}");
+        }
+    }
+
+    public void ShowDetail(string name)
+    {
+        int year = _service_book[name];
+        Console.WriteLine($"detail: {name} was repaired in >{year}<");
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()} {_AmountOfPasangers}";
+    }
+}
+
+public class Truck : Car
+{
+    private int _maximum_trucking;
+    private string _driver_Fio;
+    private int _current_weight;
+    private Dictionary<string, int> _weight_card = new Dictionary<string, int>();
+    public Truck(string name, int hp_power, int year, int trucking, string FIO, int weight) : base(name, hp_power, year)
+    {
+        _maximum_trucking = trucking;
+        _driver_Fio = FIO;
+        _current_weight = weight;
+    }
+
+    public void EditFio(string name)
+    {
+        _driver_Fio = name;
+    }
+
+    public void AddCargo(string name, int weight)
+    {
+        _weight_card.Add(name, weight);
+    }
+
+    public void DeleteCargo(string name)
+    {
+        _weight_card.Remove(name);
+    }
+
+    public void ShowCargo()
+    {
+        foreach (var cargo in _weight_card)
+        {
+            Console.WriteLine($"> {cargo.Key} weight: {cargo.Value}");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}\n >current: {_current_weight}\n >max: {_maximum_trucking}\n >FIO: {_driver_Fio}";
+    }
+}
+    
+    
---
+++ 2 homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace MyApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("BMW", 300, 2020);
+        Console.WriteLine(car.ToString());
+        Console.WriteLine();
+        
+        PassengerCar passcar = new PassengerCar("Bwm", 320, 2020, 20);
+        passcar.AddToServiceBook(2020, "wheel");
+        passcar.ShowServiceBook();
+        passcar.ShowDetail("wheel");
+        Console.WriteLine(passcar.ToString());
+        Console.WriteLine();
+        
+        Truck truck = new Truck("Mersedes", 400, 2018, 3000, "Petrov Petr Petrovich", 100);
+        truck.AddCargo("beer", 1000);
+        truck.AddCargo("snacks", 1000);
+        truck.ShowCargo();
+        Console.WriteLine();
+        truck.DeleteCargo("snacks");
+        truck.ShowCargo();
+        Console.WriteLine();
+        Console.WriteLine(truck.ToString());
+    }
+}
