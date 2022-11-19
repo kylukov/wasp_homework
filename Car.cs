2cb840b25a2b57f85a17306978f052363fd0f71e
Andrey Kulikov <aa_kylukov@vk.com>
2022-11-19 23:16:46 +0300

    Shit commit

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿namespace MyApp;
+
+public class Car
+{
+    private string _name;
+    private int _hp_power;
+    private int _year;
+    
+
+    public string Name { get => _name; set => _name = value; }
+    public int Horse_Power { get => _hp_power; set => _hp_power = value; }
+    public int Year { get => _year; set =>  _year = value; }
+    
+
+
+
+    public Car(string name, int hp_power, int year)
+    {
+        _name = name;
+        _hp_power = hp_power;
+        _year = year;
+    }
+
+    public override string ToString()
+    {
+        return $"> object: {_name} {_hp_power} {_year}";
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("BMW", 300, 2020);
+        Console.WriteLine(car.ToString());
+    }
+}
