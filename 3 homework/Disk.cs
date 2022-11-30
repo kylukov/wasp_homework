840a8dce499632d7ca3b0dbefcde96920fb9edd5
Andrey Kulikov <aa_kylukov@vk.com>
2022-11-30 15:31:39 +0300

    Add third homework

---
+++ 3 homework/Disk.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace wasp_3
+{
+	public class Disk
+	{
+		private string _name;
+		private string _genre;
+		private int _burnCount;
+
+		public string Name { get => _name; set => _name = value; }
+		public string Genre { get => _genre; set => _genre = value; }
+		public int BurnCount { get => _burnCount; set => _burnCount = value; }
+
+		public Disk(string name, string genre)
+		{
+			Name = name;
+			Genre = genre;
+		}
+
+		public virtual string[] Burn { get; set; }
+
+		public virtual int DiskSize { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Genre;
+        }
+
+    }
+
+	public class Audio: Disk
+	{
+		private string _artist;
+		private string _recordingStudio;
+		private int _songsNumber;
+
+        public string Artist { get => _artist; set => _artist = value; }
+        public string RecordingStudio { get => _recordingStudio;
+			set => _recordingStudio = value; }
+        public int SongsNumber { get => _songsNumber;
+			set => _songsNumber = value; }
+
+        public Audio(string name,
+                     string genre,
+                     string artist,
+                     string recordingStudio,
+                     int songsnumber) : base(name, genre)
+        {
+			Artist = artist;
+			RecordingStudio = recordingStudio;
+			SongsNumber = songsnumber;
+        }
+
+        public override int DiskSize { get => SongsNumber * 8; }
+
+        public override string ToString()
+        {
+            return base.Name + " " +
+                base.Genre + " " +
+				Artist + " " +
+                RecordingStudio + " " +
+                SongsNumber + " " +
+                base.Burn ;
+        }
+    }
+
+    public class DVD : Disk
+    {
+        private string _producer;
+        private string _filmCompany;
+        private int _minutesCount;
+        public string Producer { get => _producer; set => _producer = value; }
+        public string FilmCompany { get => _filmCompany; set => _filmCompany = value; }
+        public int MinutesCount { get => _minutesCount; set => _minutesCount = value; }
+        public DVD(string name,
+                   string genre,
+                   string producer,
+                   string filmCompany,
+                   int minutesCount) : base(name, genre)
+        {
+            Producer = producer;
+            FilmCompany = filmCompany;
+            MinutesCount = minutesCount;
+        }
+
+        public override int DiskSize { get => (MinutesCount / 64) * 2 ; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + Producer + " " + FilmCompany + " " + MinutesCount;
+        }
+
+    }
+
+    public class Store
+    {
+        private string _storeName;
+        private string _address;
+        private List<Audio> _audios = new List<Audio>();
+        private List<DVD> _dvds = new List<DVD>();
+
+        public string StoreName { get => _storeName; set => _storeName = value; }
+        public string Address { get => _address; set => _address = value; }
+
+        public Store(string storeName,
+                     string address)
+        {
+            StoreName = storeName;
+            Address = address;
+        }
+
+        public void AddAudio(Audio audio){ _audios.Add(audio); }
+        public void DeleteAudio(Audio audio) { _audios.Remove(audio); }
+
+        public void AddDvd(DVD dvd) { _dvds.Add(dvd); }
+        public void DeleteDVD(DVD dvd) { _dvds.Remove(dvd); }
+
+        public override string ToString()
+        {
+            string res = "";
+            foreach(Audio audio in _audios)
+            {
+                res += " > Audio: " + audio.ToString() + "\n";
+            }
+            foreach(DVD dvd in _dvds)
+            {
+                res += " > Film: " + dvd.ToString() + "\n";
+            }
+
+            return res;
+        }
+
+
+    }
+}
+
---
+++ 3 homework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace wasp_3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Audio lsp_audio = new Audio("Crazy_song", "POP", "LSP", "MSK", 1);
+        Audio pharaoh = new Audio("Crazyness", "POP", "pharaoh", "MSK", 3);
+
+        DVD dvd_filb = new DVD("Pirats of Caribean", "Traveling", "HZ", "IDK", 140);
+        DVD dvd_film = new DVD("Red Notice", "Dramma", "HZ", "IDK", 153);
+
+        Console.WriteLine(lsp_audio.ToString());
+        Console.WriteLine(dvd_filb.ToString());
+        Console.WriteLine();
+
+        Store store1 = new Store("GeekAudios+DVDs", "MSK");
+        store1.AddAudio(lsp_audio);
+        store1.AddAudio(pharaoh);
+
+        store1.AddDvd(dvd_filb);
+        store1.AddDvd(dvd_film);
+
+        Console.WriteLine(store1.ToString());
+        Console.ReadKey();
+    }
+}
